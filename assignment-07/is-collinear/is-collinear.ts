4097fc7751510f2a3639fb8e6a0c1ca315fbf994
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-10-07 23:12:21 +0530

    refactor: :wrench: amend is-collinear corner case

--- assignment-07/is-collinear/is-collinear.ts
+++ assignment-07/is-collinear/is-collinear.ts
@@ -10,10 +10,11 @@ const isCollinear = (points: Array<[number, number]>): boolean => {
     /* 
         *Using the slope formula for find if three points are collinear,
 
-        ! (y2 - y1) / (x2 - x1) === (y3 - y2) / (x3 - x2)
+        ! (y2 - y1) * (x3 - x2) === (y3 - y2) / (x1 - x0)
     */
-    return ((points[1][1] - points[0][1]) / (points[1][0] - points[0][0])) === ((points[2][1] - points[1][1]) / (points[2][0] - points[1][0]));
+    return (points[1][1] - points[0][1]) * (points[2][0] - points[1][0]) === (points[2][1] - points[1][1]) * (points[1][0] - points[0][0]);
 }
 
 console.log(isCollinear([[1, 1], [1, 6], [0, 9]]));
 console.log(isCollinear([[1, 1], [1, 4], [1, 5]]));
+console.log(isCollinear([[0, 0], [1, 4], [1, 5]]));
