23209070a82586bee40efedecb0eb7038cb5ff65
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-11-10 08:16:28 +0530

    feat: :sparkles: add solution for valid-parenthesis

--- lecture-021/valid-parenthesis.ts
+++ lecture-021/valid-parenthesis.ts
@@ -1,5 +1,26 @@
 const isValidParenthesis = (s: string): boolean => {
-    
+    const parenthesisStack: Array<string> = [];
+
+    const matcherMap: Record<string, string | undefined> = {
+        "}" : "{",
+        ")" : "(",
+        "]" : "["
+    }
+
+    for(let parenthesis of s){
+        if(parenthesis === "(" || parenthesis === "{" || parenthesis === "["){
+            parenthesisStack.push(parenthesis);
+        }
+        else if(parenthesisStack.length <= 0) return false;
+
+        else if(parenthesisStack[parenthesisStack.length - 1] === matcherMap[parenthesis]){
+            parenthesisStack.pop();
+        }
+
+        else return false;
+    }
+
+    return parenthesisStack.length === 0;
 }
 
 console.log(isValidParenthesis("()"));
