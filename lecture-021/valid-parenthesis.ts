a6ee31534ff23c689c92e90577b5e6416a06cdac
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-11-10 08:13:17 +0530

    feat: :sparkles: add solution for valid-parenthesis

---
+++ lecture-021/valid-parenthesis.ts
@@ -0,0 +1,7 @@
+const isValidParenthesis = (s: string): boolean => {
+    
+}
+
+console.log(isValidParenthesis("()"));
+console.log(isValidParenthesis("()[]{}"));
+console.log(isValidParenthesis("(]"));
